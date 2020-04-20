5766d46e4aab46feff852829251f02b275651ff0
Joep Laarhoven <laarhoven.joep@gmail.com>
2020-04-20 14:13:39 +0200

    .

---
+++ src/main/java/com/example/bp4/Bp4Application.java
@@ -0,0 +1,13 @@
+package com.example.bp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bp4Application.class, args);
+    }
+
+}
--- src/test/java/com/example/bp4/Bp4ApplicationTests.java
+++ src/test/java/com/example/bp4/Bp4ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.bp4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Bp4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
