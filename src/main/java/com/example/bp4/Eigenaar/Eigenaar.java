967f5f1e8e28188d6b8f47fe62a469901b65a507
drwvermeulen <drw.vermeulen@student.avans.nl>
2020-04-24 15:50:50 +0200

    Update Eigenaar.java

--- src/main/java/com/example/bp4/Eigenaar/Eigenaar.java
+++ src/main/java/com/example/bp4/Eigenaar/Eigenaar.java
@@ -12,23 +12,60 @@ public class Eigenaar {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer eigenaarID;
-    private String EigenaarNaam, woonplaats, telefoonnummer, email, wachtwoord;
+    private Integer eigenaar_id;
+    private String eigenaar_voornaam, eigenaar_achternaam, woonplaats, telefoonnummer, emailadres, wachtwoord;
+    private boolean verified;
 
-    public Integer getEigenaarID() {
-        return eigenaarID;
+    public Eigenaar(String eigenaar_voornaam, String eigenaar_achternaam, String woonplaats, String telefoonnummer, String emailadres, String wachtwoord, boolean verified) {
+        this.eigenaar_achternaam = eigenaar_achternaam;
+        this.eigenaar_voornaam = eigenaar_voornaam;
+        this.woonplaats = woonplaats;
+        this.telefoonnummer = telefoonnummer;
+        this.emailadres = emailadres;
+        this.wachtwoord = wachtwoord;
+        this.verified = verified;
     }
 
-    public void setEigenaarID(Integer eigenaarID) {
-        this.eigenaarID = eigenaarID;
+    public Eigenaar() {
+
     }
 
-    public String getEigenaarNaam() {
-        return EigenaarNaam;
+    public Eigenaar(int id, boolean verified) {
+        this.eigenaar_id = id;
+        this.verified = verified;
+
     }
 
-    public void setEigenaarNaam(String eigenaarNaam) {
-        EigenaarNaam = eigenaarNaam;
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public Integer getEigenaar_id() {
+        return eigenaar_id;
+    }
+
+    public void setEigenaar_id(Integer eigenaar_id) {
+        this.eigenaar_id = eigenaar_id;
+    }
+
+    public String getEigenaar_voornaam() {
+        return eigenaar_voornaam;
+    }
+
+    public void setEigenaar_voornaam(String eigenaar_voornaam) {
+        this.eigenaar_voornaam = eigenaar_voornaam;
+    }
+
+    public String getEigenaar_achternaam() {
+        return eigenaar_achternaam;
+    }
+
+    public void setEigenaar_achternaam(String eigenaar_achternaam) {
+        this.eigenaar_achternaam = eigenaar_achternaam;
     }
 
     public String getWoonplaats() {
@@ -47,12 +84,12 @@ public class Eigenaar {
         this.telefoonnummer = telefoonnummer;
     }
 
-    public String getEmail() {
-        return email;
+    public String getEmailadres() {
+        return emailadres;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmailadres(String emailadres) {
+        this.emailadres = emailadres;
     }
 
     public String getWachtwoord() {
