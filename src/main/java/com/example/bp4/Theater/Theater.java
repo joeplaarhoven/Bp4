c5e064d6d20508e99c09a244d3ee09afcf55597a
drwvermeulen <drw.vermeulen@student.avans.nl>
2020-05-05 16:55:31 +0200

    Er waren wijzigingen in de database voor de theaters tabel, deze wijzigingen aangepast in de code.

--- src/main/java/com/example/bp4/Theater/Theater.java
+++ src/main/java/com/example/bp4/Theater/Theater.java
@@ -13,64 +13,76 @@ public class Theater {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer theaterID;
+    private Integer theater_id;
     private Integer huisnummer;
     
-    @Column(name = "theaternaam", nullable = false, unique = false)
+    @Column(name = "theaternaam", nullable = false, unique = true)
     private String theaternaam;
     @Column(name = "straatnaam", nullable = false, unique = false)
     private String straatnaam;
-    @Column(name = "vestiging", nullable = false, unique = false)
-    private String vestiging;
+    @Column(name = "postcode", nullable = false, unique = false)
+    private String postcode;
+    @Column(name = "plaats", nullable = false, unique = false)
+    private String plaats;
     
-    public Theater(String theaternaam, String straatnaam, String vestiging, Integer huisnummer) {
+    public Theater(String theaternaam, String straatnaam, Integer huisnummer, String postcode, String plaats) {
     	this.theaternaam = theaternaam;
     	this.straatnaam = straatnaam;
-    	this.vestiging = vestiging;
     	this.huisnummer = huisnummer;
+    	this.postcode = postcode;
+    	this.plaats = plaats;
     }
     
     public Theater() {
     	
     }
 
-    public Integer getTheaterID() {
-        return theaterID;
-    }
+	public Integer getTheater_id() {
+		return theater_id;
+	}
 
-    public void setTheaterID(Integer theaterID) {
-        this.theaterID = theaterID;
-    }
+	public void setTheater_id(Integer theater_id) {
+		this.theater_id = theater_id;
+	}
 
-    public Integer getHuisnummer() {
-        return huisnummer;
-    }
+	public Integer getHuisnummer() {
+		return huisnummer;
+	}
 
-    public void setHuisnummer(Integer huisnummer) {
-        this.huisnummer = huisnummer;
-    }
+	public void setHuisnummer(Integer huisnummer) {
+		this.huisnummer = huisnummer;
+	}
 
-    public String getTheaternaam() {
-        return theaternaam;
-    }
+	public String getTheaternaam() {
+		return theaternaam;
+	}
 
-    public void setTheaternaam(String theaternaam) {
-        this.theaternaam = theaternaam;
-    }
+	public void setTheaternaam(String theaternaam) {
+		this.theaternaam = theaternaam;
+	}
 
-    public String getStraatnaam() {
-        return straatnaam;
-    }
+	public String getStraatnaam() {
+		return straatnaam;
+	}
 
-    public void setStraatnaam(String straatnaam) {
-        this.straatnaam = straatnaam;
-    }
+	public void setStraatnaam(String straatnaam) {
+		this.straatnaam = straatnaam;
+	}
 
-    public String getVestiging() {
-        return vestiging;
-    }
+	public String getPostcode() {
+		return postcode;
+	}
 
-    public void setVestiging(String vestiging) {
-        this.vestiging = vestiging;
-    }
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getPlaats() {
+		return plaats;
+	}
+
+	public void setPlaats(String plaats) {
+		this.plaats = plaats;
+	}
+	
 }
